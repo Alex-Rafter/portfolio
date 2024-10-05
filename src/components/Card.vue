ca9aa7c241d614289dffab4a52c861558216b5f2
Alex Rafter <alexrafteremail@gmail.com>
2024-10-05 22:12:20 +0100

    added title sfc to test

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+	  <h1>{{ title }}</h1>
+	  <p>This is a card component</p>
+	</div>
+  </template>
+
+  <script>
+  export default {
+	name: 'TitleComponent',
+	props: {
+	  title: {
+		type: String,
+		required: true
+	  }
+	}
+  }
+  </script>
+
+  <style scoped>
+  h1 {
+	font-size: 2em;
+	color: #333;
+  }
+  </style>
